554d717ef1fec4827b1a79567c4618e0293c0b8e
Edvard <edvard@MacBook-Pro-Edvard.local>
2026-06-04 21:37:57 +0600

    Update place modal design, fix images paths and other bugs

--- public/places-data.js
+++ public/places-data.js
@@ -5,13 +5,13 @@ const BISHKEK_PLACES = [
         "tag": "Прогулки",
         "category": "парк",
         "desc": "Самый любимый бульвар горожан. Идеальное место для неспешных прогулок под тенью деревьев.",
-        "img": "../img/place_1_1.jpg",
+        "img": "/img/place_1_1.jpg",
         "photos": [
-            "../img/place_1_1.jpg",
-            "../img/place_1_2.jpg",
-            "../img/place_1_3.jpg",
-            "../img/place_1_4.jpg",
-            "../img/place_1_5.jpg"
+            "/img/place_1_1.jpg",
+            "/img/place_1_2.jpg",
+            "/img/place_1_3.jpg",
+            "/img/place_1_4.jpg",
+            "/img/place_1_5.jpg"
         ],
         "address": "бул. Эркиндик",
         "gisLink": "https://2gis.kg/bishkek/geo/15763234351059920",
@@ -25,13 +25,13 @@ const BISHKEK_PLACES = [
         "tag": "Торговый центр",
         "category": "трц",
         "desc": "Популярный торговый центр в самом сердце города с магазинами, фуд-кортом и развлечениями.",
-        "img": "../img/place_2_1.jpg",
+        "img": "/img/place_2_1.jpg",
         "photos": [
-            "../img/place_2_1.jpg",
-            "../img/place_2_2.jpg",
-            "../img/place_2_3.jpg",
-            "../img/place_2_4.jpg",
-            "../img/place_2_5.jpg"
+            "/img/place_2_1.jpg",
+            "/img/place_2_2.jpg",
+            "/img/place_2_3.jpg",
+            "/img/place_2_4.jpg",
+            "/img/place_2_5.jpg"
         ],
         "address": "ул. Киевская, 148",
         "gisLink": "https://2gis.kg/bishkek/search/bishkek park",
@@ -45,13 +45,13 @@ const BISHKEK_PLACES = [
         "tag": "Горы и хайкинг",
         "category": "природа",
         "desc": "Национальный природный парк всего в 40 минутах от города. Величественные горы, водопады и свежий воздух.",
-        "img": "../img/place_3_1.jpg",
+        "img": "/img/place_3_1.jpg",
         "photos": [
-            "../img/place_3_1.jpg",
-            "../img/place_3_2.jpg",
-            "../img/place_3_3.jpg",
-            "../img/place_3_4.jpg",
-            "../img/place_3_5.jpg"
+            "/img/place_3_1.jpg",
+            "/img/place_3_2.jpg",
+            "/img/place_3_3.jpg",
+            "/img/place_3_4.jpg",
+            "/img/place_3_5.jpg"
         ],
         "address": "Кыргызстан, Чуйская область",
         "gisLink": "https://2gis.kg/bishkek/geo/15763234351061234",
@@ -65,13 +65,13 @@ const BISHKEK_PLACES = [
         "tag": "Природа и искусство",
         "category": "парк",
         "desc": "Старейший парк города. Здесь расположена галерея скульптур под открытым небом и вековые дубы.",
-        "img": "../img/place_4_1.jpg",
+        "img": "/img/place_4_1.jpg",
         "photos": [
-            "../img/place_4_1.jpg",
-            "../img/place_4_2.jpg",
-            "../img/place_4_3.jpg",
-            "../img/place_4_4.jpg",
-            "../img/place_4_5.jpg"
+            "/img/place_4_1.jpg",
+            "/img/place_4_2.jpg",
+            "/img/place_4_3.jpg",
+            "/img/place_4_4.jpg",
+            "/img/place_4_5.jpg"
         ],
         "address": "ул. Пушкина / ул. Тыныстанова",
         "gisLink": "https://2gis.kg/bishkek/geo/15763234351059918",
@@ -85,13 +85,13 @@ const BISHKEK_PLACES = [
         "tag": "Жемчужина Азии",
         "category": "природа",
         "desc": "Высокогорное озеро, одно из крупнейших и глубочайших в мире. Идеальное место для летнего отдыха и туризма.",
-        "img": "../img/place_5_1.jpg",
+        "img": "/img/place_5_1.jpg",
         "photos": [
-            "../img/place_5_1.jpg",
-            "../img/place_5_2.jpg",
-            "../img/place_5_3.jpg",
-            "../img/place_5_4.jpg",
-            "../img/place_5_5.jpg"
+            "/img/place_5_1.jpg",
+            "/img/place_5_2.jpg",
+            "/img/place_5_3.jpg",
+            "/img/place_5_4.jpg",
+            "/img/place_5_5.jpg"
         ],
         "address": "Иссык-Кульская область",
         "gisLink": "https://2gis.kg/bishkek/search/иссык-куль",
@@ -105,13 +105,13 @@ const BISHKEK_PLACES = [
         "tag": "Завтраки и кофе",
         "category": "кафе",
         "desc": "Уютное место для завтраков и душевных встреч. Свежая выпечка и отличный кофе в центре Бишкека.",
-        "img": "../img/place_6_1.jpg",
+        "img": "/img/place_6_1.jpg",
         "photos": [
-            "../img/place_6_1.jpg",
-            "../img/place_6_2.jpg",
-            "../img/place_6_3.jpg",
-            "../img/place_6_4.jpg",
-            "../img/place_6_5.jpg"
+            "/img/place_6_1.jpg",
+            "/img/place_6_2.jpg",
+            "/img/place_6_3.jpg",
+            "/img/place_6_4.jpg",
+            "/img/place_6_5.jpg"
         ],
         "address": "бул. Эркиндик, 35",
         "gisLink": "https://2gis.kg/bishkek/search/бублик",
@@ -125,13 +125,13 @@ const BISHKEK_PLACES = [
         "tag": "Современный парк",
         "category": "парк",
         "desc": "Новый современный парк с красивыми аллеями, зонами для отдыха и красивой вечерней подсветкой.",
-        "img": "../img/place_7_1.jpg",
+        "img": "/img/place_7_1.jpg",
         "photos": [
-            "../img/place_7_1.jpg",
-            "../img/place_7_2.jpg",
-            "../img/place_7_3.jpg",
-            "../img/place_7_4.jpg",
-            "../img/place_7_5.jpg"
+            "/img/place_7_1.jpg",
+            "/img/place_7_2.jpg",
+            "/img/place_7_3.jpg",
+            "/img/place_7_4.jpg",
+            "/img/place_7_5.jpg"
         ],
         "address": "Южная магистраль",
         "gisLink": "https://2gis.kg/bishkek/search/евразия",
@@ -145,13 +145,13 @@ const BISHKEK_PLACES = [
         "tag": "Спорт и отдых",
         "category": "парк",
         "desc": "Отличное место для активного отдыха, скейтбординга и семейных прогулок в южной части города.",
-        "img": "../img/place_8_1.jpg",
+        "img": "/img/place_8_1.jpg",
         "photos": [
-            "../img/place_8_1.jpg",
-            "../img/place_8_2.jpg",
-            "../img/place_8_3.jpg",
-            "../img/place_8_4.jpg",
-            "../img/place_8_5.jpg"
+            "/img/place_8_1.jpg",
+            "/img/place_8_2.jpg",
+            "/img/place_8_3.jpg",
+            "/img/place_8_4.jpg",
+            "/img/place_8_5.jpg"
         ],
         "address": "пр. Чингиза Айтматова",
         "gisLink": "https://2gis.kg/bishkek/search/ынтымак",
@@ -165,13 +165,13 @@ const BISHKEK_PLACES = [
         "tag": "Центр города",
         "category": "парк",
         "desc": "Центральная площадь Бишкека, сердце города. Здесь проходят все главные праздники, военные парады и народные гуляния.",
-        "img": "../img/place_9_1.jpg",
+        "img": "/img/place_9_1.jpg",
         "photos": [
-            "../img/place_9_1.jpg",
-            "../img/place_9_2.jpg",
-            "../img/place_9_3.jpg",
-            "../img/place_9_4.jpg",
-            "../img/place_9_5.jpg"
+            "/img/place_9_1.jpg",
+            "/img/place_9_2.jpg",
+            "/img/place_9_3.jpg",
+            "/img/place_9_4.jpg",
+            "/img/place_9_5.jpg"
         ],
         "address": "просп. Чуй",
         "gisLink": "https://2gis.kg/bishkek/geo/70030076150117070",
@@ -185,13 +185,13 @@ const BISHKEK_PLACES = [
         "tag": "Природа",
         "category": "природа",
         "desc": "Живописное ущелье с альпийскими лугами и современными горнолыжными базами. Отлично подходит для отдыха в любой сезон.",
-        "img": "../img/place_10_1.jpg",
+        "img": "/img/place_10_1.jpg",
         "photos": [
-            "../img/place_10_1.jpg",
-            "../img/place_10_2.jpg",
-            "../img/place_10_3.jpg",
-            "../img/place_10_4.jpg",
-            "../img/place_10_5.jpg"
+            "/img/place_10_1.jpg",
+            "/img/place_10_2.jpg",
+            "/img/place_10_3.jpg",
+            "/img/place_10_4.jpg",
+            "/img/place_10_5.jpg"
         ],
         "address": "с. Таш-Добо",
         "gisLink": "https://2gis.kg/bishkek/geo/15763234351061235",
@@ -205,13 +205,13 @@ const BISHKEK_PLACES = [
         "tag": "Шопинг",
         "category": "трц",
         "desc": "Самый современный ТРЦ города. Брендовые магазины, фуд-корт с кухнями мира и отличный кинотеатр.",
-        "img": "../img/place_11_1.jpg",
+        "img": "/img/place_11_1.jpg",
         "photos": [
-            "../img/place_11_1.jpg",
-            "../img/place_11_2.jpg",
-            "../img/place_11_3.jpg",
-            "../img/place_11_4.jpg",
-            "../img/place_11_5.jpg"
+            "/img/place_11_1.jpg",
+            "/img/place_11_2.jpg",
+            "/img/place_11_3.jpg",
+            "/img/place_11_4.jpg",
+            "/img/place_11_5.jpg"
         ],
         "address": "просп. Ч. Айтматова, 3",
         "gisLink": "https://2gis.kg/bishkek/search/asia mall",
@@ -225,13 +225,13 @@ const BISHKEK_PLACES = [
         "tag": "Кино",
         "category": "трц",
         "desc": "Один из лучших кинотеатров в Бишкеке, расположенный в крупном торговом комплексе Dordoi Plaza. IMAX и комфортные залы.",
-        "img": "../img/place_12_1.jpg",
+        "img": "/img/place_12_1.jpg",
         "photos": [
-            "../img/place_12_1.jpg",
-            "../img/place_12_2.jpg",
-            "../img/place_12_3.jpg",
-            "../img/place_12_4.jpg",
-            "../img/place_12_5.jpg"
+            "/img/place_12_1.jpg",
+            "/img/place_12_2.jpg",
+            "/img/place_12_3.jpg",
+            "/img/place_12_4.jpg",
+            "/img/place_12_5.jpg"
         ],
         "address": "ул. Ибраимова, 115",
         "gisLink": "https://2gis.kg/bishkek/search/дордой плаза кино",
@@ -245,13 +245,13 @@ const BISHKEK_PLACES = [
         "tag": "Национальная кухня",
         "category": "кафе",
         "desc": "Традиционная кыргызская кухня в богатом национальном интерьере. Отличное место для знакомства с культурой.",
-        "img": "../img/place_13_1.jpg",
+        "img": "/img/place_13_1.jpg",
         "photos": [
-            "../img/place_13_1.jpg",
-            "../img/place_13_2.jpg",
-            "../img/place_13_3.jpg",
-            "../img/place_13_4.jpg",
-            "../img/place_13_5.jpg"
+            "/img/place_13_1.jpg",
+            "/img/place_13_2.jpg",
+            "/img/place_13_3.jpg",
+            "/img/place_13_4.jpg",
+            "/img/place_13_5.jpg"
         ],
         "address": "ул. Тоголок Молдо, 114",
         "gisLink": "https://2gis.kg/bishkek/search/navat",
@@ -265,13 +265,13 @@ const BISHKEK_PLACES = [
         "tag": "Ночная жизнь",
         "category": "паб",
         "desc": "Легендарный рок-клуб Бишкека. Живой звук, отличная кухня и неповторимая атмосфера свободы.",
-        "img": "../img/place_14_1.jpg",
+        "img": "/img/place_14_1.jpg",
         "photos": [
-            "../img/place_14_1.jpg",
-            "../img/place_14_2.jpg",
-            "../img/place_14_3.jpg",
-            "../img/place_14_4.jpg",
-            "../img/place_14_5.jpg"
+            "/img/place_14_1.jpg",
+            "/img/place_14_2.jpg",
+            "/img/place_14_3.jpg",
+            "/img/place_14_4.jpg",
+            "/img/place_14_5.jpg"
         ],
         "address": "ул. Чолпон-Атинская, 16",
         "gisLink": "https://2gis.kg/bishkek/search/promzona",
@@ -285,13 +285,13 @@ const BISHKEK_PLACES = [
         "tag": "Кофейня",
         "category": "кафе",
         "desc": "Стильная и современная кофейня для работы и встреч. Прекрасный интерьер, спешалти кофе и авторские десерты.",
-        "img": "../img/place_15_1.jpg",
+        "img": "/img/place_15_1.jpg",
         "photos": [
-            "../img/place_15_1.jpg",
-            "../img/place_15_2.jpg",
-            "../img/place_15_3.jpg",
-            "../img/place_15_4.jpg",
-            "../img/place_15_5.jpg"
+            "/img/place_15_1.jpg",
+            "/img/place_15_2.jpg",
+            "/img/place_15_3.jpg",
+            "/img/place_15_4.jpg",
+            "/img/place_15_5.jpg"
         ],
         "address": "ул. Токтогула, 125",
         "gisLink": "https://2gis.kg/bishkek/search/capito",
