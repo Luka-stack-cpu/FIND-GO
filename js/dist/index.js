941a09ba4b243f10fe269fe333da269bae1b9402
Edvard <edvard@MacBook-Pro-Edvard.local>
2026-08-01 14:14:42 +0600

    fix: add root index.html and static assets for GitHub Pages

---
+++ js/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DangerousPlacesSlider = exports.SafetyBadge = exports.SafetyTooltip = void 0;
+var SafetyTooltip_js_1 = require("./SafetyTooltip.js");
+Object.defineProperty(exports, "SafetyTooltip", { enumerable: true, get: function () { return SafetyTooltip_js_1.SafetyTooltip; } });
+var SafetyBadge_js_1 = require("./SafetyBadge.js");
+Object.defineProperty(exports, "SafetyBadge", { enumerable: true, get: function () { return SafetyBadge_js_1.SafetyBadge; } });
+var DangerousPlacesSlider_js_1 = require("./DangerousPlacesSlider.js");
+Object.defineProperty(exports, "DangerousPlacesSlider", { enumerable: true, get: function () { return DangerousPlacesSlider_js_1.DangerousPlacesSlider; } });
