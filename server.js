3e451a65125f8f7ce7254e06c637bfe83582253b
Edvard <edvard@MacBook-Pro-Edvard.local>
2026-08-05 00:57:07 +0600

    fix: update project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
